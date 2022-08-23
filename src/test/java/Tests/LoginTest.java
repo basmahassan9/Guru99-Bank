9823547b9c90ad86a1067ad58cf17ab7f6ad7b13
basmahassan9 <basmahassan.201096@gmail.com>
2022-08-24 00:07:12 +0200

    day 5

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import base.baseTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import utils.readXLFiles;
@@ -19,5 +20,25 @@ public class LoginTest extends baseTest {
     soft.assertEquals(login.getText(),"Welcome To Manager's Page of Guru99 Bank","wrong text");
     soft.assertEquals(driver.getCurrentUrl(),"https://www.demo.guru99.com/V4/manager/Managerhomepage.php","wrong URL");
     soft.assertAll();
+    }
+    @Test(dataProvider = "loginInfo")
+    public void loginTest2(String username, String password){
+        System.out.println(username);
+        System.out.println(password);
+
+        login.addUserName(username);
+        login.addPaddword(password);
+        login.clickLoginBtn();
+        SoftAssert soft = new SoftAssert();
+        soft.assertEquals(login.getText(),"Welcome To Manager's Page of Guru99 Bank","wrong text");
+        soft.assertEquals(driver.getCurrentUrl(),"https://www.demo.guru99.com/V4/manager/Managerhomepage.php","wrong URL");
+        soft.assertAll();
+    }
+
+
+
+    @DataProvider(name = "loginInfo")
+    public Object[][] loginInfo(){
+        return new Object[][]{{"mngr432952","atUjUra"},{"emngr432952","atUjUrae"},{"mngr432952","eatUjUra"},{"mngr43e2952e","atUjUra"}};
     }
 }
